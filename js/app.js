2a442ecd5805da126ec66c0198754b6bd8da884c
Clarinda Bui <clarindabui@outlook.com>
2018-06-29 16:53:24 -0700

    Render enemies on the board

--- js/app.js
+++ js/app.js
@@ -5,7 +5,7 @@ class Entity {
         this.x = 2;
         this.y = 5;
     }
-
+    // Draws the entity on the board
     render() {
         ctx.drawImage(Resources.get(this.sprite), this.x * 101, this.y * 83);
     }
@@ -22,30 +22,6 @@ class Enemy extends Entity {
     }
 }
 
-// Enemies our player must avoid
-var Enemy = function() {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
-
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
-    this.sprite = 'images/enemy-bug.png';
-};
-
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
-Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
-};
-
-// Draw the enemy on the screen, required method for game
-Enemy.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-};
-
-
 
 // Now write your own player class
 class Player extends Entity {
@@ -55,16 +31,18 @@ class Player extends Entity {
     }
 }
 
-const player = new Player();
-
 // This class requires an update(), render() and
 // a handleInput() method.
 
 
 // Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
 
+// Place all enemy objects in an array called allEnemies
+//Create an array of three enemies
+const allEnemies = [...Array(3)].map((_,i)=> new Enemy(0,i+1));
+
+// Place the player object in a variable called player
+const player = new Player();
 
 
 // This listens for key presses and sends the keys to your
